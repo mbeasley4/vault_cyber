380830cd329c3a7dfa0fa2bb1cafc2c59fd006b8
Mike Beasley <mbeasley4@gmail.com>
2026-01-30 15:52:47 -0500

    minor updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
       colors: {
         cyber: {
           green: "#00ff9c",
